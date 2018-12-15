0e83dba6a2fb2ad6a738b7c32812ade226001a6f
amanpabla <amanpabla@gmail.com>
2018-12-15 09:41:49 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toDateString();
+    var log = `${now}: ${req.method}: ${req.path}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+})
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.get('/', (req, res) => {
+    res.send('welcome');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear(),
+    });
+});
+
+app.get('/home', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the Home Page of my website',
+        currentYear: new Date().getFullYear(),
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+})
